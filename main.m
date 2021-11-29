bff43ba1972c3794544b8afe31f691118d006fcc
Conner Martin <coma6440@colorado.edu>
2021-11-29 15:53:32 -0700

    Added placeholders for leunberger observer

--- main.m
+++ main.m
@@ -31,3 +31,23 @@ P = ctrb(A,B);
 O = obsv(A,C);
 fprintf("Rank of controllability matrix = %d\n",rank(P));
 fprintf("Rank of observability matrix = %d\n", rank(O));
+
+%% Closed Loop
+P = [-1,-2,-3,-4,-5,-6];
+K = place(A,B,P);
+
+F = eye(3);
+%% Luenberger observer
+P_L = [-1, -2, -3, -4, -5, -6];
+L = place(A', C', P_L)';
+
+A_cl_aug = [(A-B*K), B*K; zeros(6), (A-L*C)];
+B_cl_aug = [B*F;zeros(6,3)];
+C_cl_aug = [C, zeros(3,6)];
+D_cl_aug = 0;
+
+sys_CL_OBS = ss(A_cl_aug, B_cl_aug, C_cl_aug, D_cl_aug);
+
+% Simulate with zero initial error
+
+% Simulate with non-zero initial error
